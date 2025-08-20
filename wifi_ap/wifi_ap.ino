9d9ef0983dea88bf6c4049033943fcc960ac64ee
Raphael Ramos <raphael.opensource@gmail.com>
2025-08-20 06:56:39 -0300

    Upload Makerfabs code with comments translated

--- wifi_ap/wifi_ap.ino
+++ wifi_ap/wifi_ap.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "wifi_save.h"
 
+void setup()
+{
+    Serial.begin(115200);
+    if (wifi_set_main())
+    {
+        Serial.println("Connect WIFI SUCCESS");
+    }
+    else
+    {
+        Serial.println("Connect WIFI FAULT");
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+    Serial.println("loop");
+    delay(3000);
 }
---
+++ wifi_ap/wifi_save.h
@@ -0,0 +1,46 @@
+/*
+
+Author  : Vincent
+Create  : 2021/2/5
+
+if ESP32 startup detects SSID == NULL:
+    ESP32 enters AP mode, access 192.168.4.1 to modify and obtain SSID and PASSWORD
+    Store them in FLASH via NVS
+    Restart
+else:
+    Connect to Wi-Fi using SSID and PASSWORD from FLASH
+
+while within 5 seconds after restart && reset button is pressed:
+    Set NVS to NULL
+    Restart
+
+user program()
+
+*/
+
+#include <Arduino.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+
+#define SSID_LENGTH 40
+
+#define WIFI_SET_PIN 21
+
+int record_rst_time();
+void nvs_test();
+void record_wifi(char *ssid, char *password);
+void check_wifi(char *ssid, char *password);
+
+void ap_init();
+int wifi_config_server();
+void set_wifi_from_url(String get_url);
+int wifi_set_main();
